ac2eb931a083ed0d742bebf92824afd6c8a144f7
Brittany Brundin  & Carlo Francisco & Thomas Feng <pair+bbrundin.cfrancisco726.realtomfeng@devbootcamp.com>
2017-04-14 15:28:04 -0400

    finished

--- nyc_jobs.rb
+++ nyc_jobs.rb
@@ -13,7 +13,6 @@ parsed_data = JSON.parse(data, headers: true, header_converters: :symbol)
 puts "Below are the current departments with open positions.".yellow
 puts ""
 departments = parsed_data.map do |element|
-  # element.default = "Keys not defined"
   element['agency'].blue
 end
 
@@ -27,31 +26,16 @@ puts "What department are you interested in?".yellow
 searched_industry = gets.chomp
 puts ""
 
-# choices = parsed_data.select do |element|
-#   # element.default = "Keys not defined"
-#   searched_industry.downcase == element['agency'].downcase
-# end
-# if choices.empty?
-#   puts "please enter another choice."
-# else
-
-
-
-
 puts "Here are the available jobs in the #{searched_industry}.".yellow
 
 # parsed_data = JSON.parse(data, headers: true, header_converters: :symbol)
 puts "\n"
 parsed_data.each do |element|
-  # element.default = "Keys not defined"
   if searched_industry.downcase == element['agency'].downcase
     puts "#{element['business_title']}, #{element['civil_service_title']} $#{element['salary_range_to']}.".blue
 
   end
 end
 
-#look for industry
-#look for job titles
-#look for job salaries
-
+puts Emoji.find_by_alias("moneybag").raw
 
